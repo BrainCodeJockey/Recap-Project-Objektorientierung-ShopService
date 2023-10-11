bed8d5550ec1849944e2dd3ec1e2ad207624cdb4
Alexander Unmuth <63350892+Hirnstromwelle@users.noreply.github.com>
2023-10-11 12:13:06 +0200

    Coding optional product (#6)
    * add optional Product
    
    * add optional Product

--- src/main/java/OrderStatus.java
+++ src/main/java/OrderStatus.java
@@ -1,5 +1,17 @@
 public enum OrderStatus {
-    PROCESSING,
-    IN_DELIVERY,
-    COMPLETED;
+    PROCESSING("progressing"),
+    IN_DELIVERY("in delivery"),
+    COMPLETED("compleded");
+
+    public final String orderStatus;
+
+    OrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
 }
+
+
--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,15 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
 
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
 
+    // Methode, um alle Bestellungen mit einem bestimmten Bestellstatus zurückzugeben
+    public List<Order> getOrdersByStatus(OrderStatus orderStatus) {
+        List<Order> byStatus = orderRepo.getOrders().stream()
+                .filter(order -> order.status().equals(orderStatus))
+                .collect(Collectors.toList());
+
+        return byStatus;
+    }
+
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
+            Optional<Product> productToOrder = productRepo.getProductById(productId);
             if (productToOrder == null) {
                 System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
                 return null;
@@ -17,7 +29,7 @@ public class ShopService {
             products.add(productToOrder);
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+        Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
 
         return orderRepo.addOrder(newOrder);
     }
